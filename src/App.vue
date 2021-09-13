398c46a7c743883dc6ecac0b9dc6bc89938c254e
sblockwel <63020705+sblockwel@users.noreply.github.com>
2021-09-13 20:59:00 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,19 @@
       </div>
     </form>
 
+    <div>
+      <label>Filtrar</label><br/>
+      <input type="text" id="nome" >
+      <button>Pesquisar</button>
+    </div>
+
     <div >
       <table class="tabela">
-        <tr>
-          <th>Nome</th>
-          <th>Email</th>
-          <th></th>
-        </tr>
-        <tr v-for="(p, idx) in participantes" :key="p.nome" :class="{ 'alteracao': alteracaoIdx == idx }">
+        <th><a href="#" @click="ordenar('nome')">Nome</a></th>
+        <th>Email</th>
+        <th>Opções</th>
+        
+        <tr v-for="(p, idx) in participantes" :key="p.nome" :class="{ 'alteracao': alteracaoIdx == idx }" class="linhas">
           <td>{{ p.nome }}</td>
           <td>{{ p.email }}</td>
           <td>
@@ -82,6 +87,11 @@ export default {
     alterar(idx) {
       this.participante = Object.assign({}, this.participantes[idx])
       this.alteracaoIdx = idx
+    },
+    ordenar(chave) {
+      this.participantes.sort(function(a, b) {
+        return a[chave].localeCompare(b[chave])
+      })
     }
   },
   computed: {
@@ -132,6 +142,21 @@ export default {
 
 .tabela {
   margin: auto;
+}
+
+table {
+    border-collapse: collapse;
+    width: 50%;
+}
+
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+
+tr:hover{
+  background-color:#f5f5f5
 }
 
 </style>
